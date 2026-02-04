d60f916677bbf1928d7d9f057e94e5f66791cc73
Christopher Obi-Gabr <christopher@example.com>
2026-02-05 08:24:58 +0930

    feat: Add CORS configuration to allow specific origins, methods, and headers for API endpoints.

--- src/main/java/com/aptechph/financial_dashboard/config/CorsConfig.java
+++ src/main/java/com/aptechph/financial_dashboard/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8082", "http://localhost:5173", "http://127.0.0.1:5173", "http://127.0.0.1:8082")
+                .allowedOrigins("http://localhost:8082", "http://localhost:5173", "http://127.0.0.1:5173", "http://127.0.0.1:8082", "http://localhost:3001","http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
